8af7d3ba613f327383652bd29b350cbea354cee6
farismelrayes <46490210+MatthewLKomar@users.noreply.github.com>
2023-04-04 16:13:59 -0400

    added psuedo code for future platform height calculations.

--- Assets/_Scripts/MineCart.cs
+++ Assets/_Scripts/MineCart.cs
@@ -6,17 +6,40 @@ public class MineCart : MonoBehaviour
 {
     // Start is called before the first frame update
     private FloorController floor = null;
+    public Transform Front;
+    public Transform Back;
+
+    private Vector3 FrontLastPosition;
+    private Vector3 BackLastPosition;
+    private Vector3 FrontCurrentPosition;
+    private Vector3 BackCurrentPosition;
 
     void Start()
     {
         floor = FloorController.curr;
         floor.SetupFloor();
-        floor.raiseAll(5.0f);
+//      floor.raiseAll(5.0f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate() {
+        RaycastHit hit;
         
+        // this should shoot a ray downwards... 
+        if (Physics.Raycast(Front.position, transform.TransformDirection(Vector3.down), out hit, 10.0f)) {
+            FrontCurrentPosition = hit.point;
+        }
+        if (Physics.Raycast(Back.position, transform.TransformDirection(Vector3.down), out hit, 10.0f)) {
+            BackCurrentPosition = hit.point;
+        }
+
+        //do calculations accordingly... 
+        // if the value to raie the floor hasn't changed since last frame
+            //return
+        // if we're going up, we'll have to move raise the ass end of the floor down, the front up. 
+        // if we're going down, we'll have to raise the ass end of the floor up, the front down. 
+        
+        
+    
     }
 }
