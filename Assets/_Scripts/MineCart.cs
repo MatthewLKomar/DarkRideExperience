099d6f84d91777471c2df5619fd4a8ddc941ff4d
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2023-04-06 15:01:47 -0400

    can now raise floor when moving along spline

--- Assets/_Scripts/MineCart.cs
+++ Assets/_Scripts/MineCart.cs
@@ -6,38 +6,64 @@ public class MineCart : MonoBehaviour
 {
     // Start is called before the first frame update
     private FloorController floor = null;
+
+    public float LengthThreshold = 2.0f;
     public Transform Front;
     public Transform Back;
 
-    private Vector3 FrontLastPosition;
-    private Vector3 BackLastPosition;
-    private Vector3 FrontCurrentPosition;
-    private Vector3 BackCurrentPosition;
-    private float oldSlope = 0;
+    private float FrontLastHeight;
+    private float BackLastHeight;
+    private float FrontCurrentHeight;
+    private float BackCurrentHeight;
+    private float LengthOfMinecart;
+    float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
     void Start()
     {
         floor = FloorController.curr;
-        floor.SetupFloor();
-//      floor.raiseAll(5.0f);
+        if (floor == null)
+        {
+            floor.SetupFloor();
+            floor.raiseAll(5.0f);
+        }
+        
+
+        FrontCurrentHeight = Front.position.y;
+        BackCurrentHeight = Back.position.y;
+
+        FrontLastHeight = FrontCurrentHeight;
+        BackLastHeight = BackCurrentHeight;
+
+        LengthOfMinecart = Vector3.Distance(Front.position, Back.position);
+        LengthOfMinecart /= LengthThreshold;
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        RaycastHit hit;
+        if (floor == null)
+            return;
+        FrontCurrentHeight = Front.position.y;
+        BackCurrentHeight = Back.position.y;
+        
+        var frontHeight = Mathf.Abs(FrontCurrentHeight - FrontLastHeight);
+        var lastHeight = Mathf.Abs(BackCurrentHeight - BackLastHeight);
 
-        // this should shoot a ray downwards... 
-        Debug.DrawRay(Front.position, transform.TransformDirection(Vector3.down));
-        if (Physics.Raycast(Front.position, transform.TransformDirection(Vector3.down), out hit, 10.0f)) {
-            FrontCurrentPosition = hit.point;
-        }
 
-        Debug.DrawRay(Back.position, transform.TransformDirection(Vector3.down));
-        if (Physics.Raycast(Back.position, transform.TransformDirection(Vector3.down), out hit, 10.0f)) {
-            BackCurrentPosition = hit.point;
-        }
 
-        //compare distance from front and back
+
+        //compare height from front and back to last frame's
         //then remap it 
-    
+        var frontVoltage = Remap(frontHeight, 0, LengthOfMinecart, 0 , 10);
+        var backVoltage = Remap(lastHeight, 0, LengthOfMinecart, 0, 10);
+
+        //print("front voltage: " + frontVoltage + " back Voltage: " + backVoltage + "front height " + frontHeight);
+        floor.raiseBack(backVoltage);
+        floor.raiseFront(frontVoltage);
+
+        FrontLastHeight = FrontCurrentHeight;
+        BackLastHeight = BackCurrentHeight;
     }
 }
