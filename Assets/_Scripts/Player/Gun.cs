d710e4740546491d51213ccf4e2f60f87687bb86
Streakman <matthew_komar@yahoo.com>
2023-05-02 01:59:17 -0400

    gun shot implemented.

--- Assets/_Scripts/Player/Gun.cs
+++ Assets/_Scripts/Player/Gun.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Update is called once per frame
+    public AudioClip audio; 
+    private AudioSource audioSource = null;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
         Debug.DrawLine(transform.position, transform.TransformDirection(Vector3.left) * 500.0f + transform.position);
         if (/*Input.GetAxis("P1-RightBumper") != 0 || */Input.GetMouseButtonDown(0)) {
             bool hit = Physics.Raycast(transform.position,transform.TransformDirection(Vector3.left), out RaycastHit impact);
             if (hit && impact.transform.tag == "Enemy") {
+                audioSource.PlayOneShot(audio);
                 Destroy(impact.transform.gameObject);
             }
         }
